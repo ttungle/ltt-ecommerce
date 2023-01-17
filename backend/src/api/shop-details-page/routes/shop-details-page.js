a878aeb5eb95fd651a09b03e409b9f32db7ddabe
Tung Le <thanhtungle73@gmail.com>
2023-01-17 23:44:52 +0700

    Added shop details page single collection

---
+++ backend/src/api/shop-details-page/routes/shop-details-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * shop-details-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::shop-details-page.shop-details-page');
